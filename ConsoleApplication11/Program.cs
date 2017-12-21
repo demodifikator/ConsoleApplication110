bc4f4fc688b8a19f3355583f09dd9cbec2f450b8
student <student@D40>
2017-12-21 19:30:17 +0200

    ,kl

--- ConsoleApplication11/Program.cs
+++ ConsoleApplication11/Program.cs
@@ -8,8 +8,51 @@ namespace ConsoleApplication11
 {
     class Program
     {
+        struct Human
+        {
+            public string Name;
+            public int Age;
+
+            public string GetAge()
+            {
+                return Age.ToString();
+            }
+        }
+
+        static public int GetAge(params int[] array)
+        {
+            //if (list.Count < 16)
+            //    return 0;
+
+            //list = new List<int>()
+            //{
+
+            //};
+
+            //return 15;
+        }
+
+
         static void Main(string[] args)
         {
+            //Human human = new Human();
+            //human.Age = 15;
+            //string age = human.GetAge();
+            //Console.WriteLine(age);
+
+            //List<int> array = new List<int>
+            //{
+            //    2,4,6
+            //};
+
+            //int x = 14;
+            //GetAge(ref x);
+
+            //Console.WriteLine(x);
+
+            //List<int> h = GetAge(ref array);
+
+            GetAge(2, 3, 4, 5, 6, 7, 8, 9, 0, 1);
         }
     }
 }
